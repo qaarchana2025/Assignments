709deaead44cac30607d5284c5d3f184a62b57ef
Archana <qa.archana2025@outlook.com>
2025-11-23 22:45:10 +0530

    Assignments

---
+++ src/Assignment/Assignment13_Powernumber.java
@@ -0,0 +1,22 @@
+package Assignment;
+
+public class Assignment13_Powernumber {
+
+	public static void main(String[] args) {
+		
+		double output=1.00;
+		double x=2.00000;
+		double n=(-2);
+		
+		
+		
+		for(int i = 1; i <= n; i++){
+	        output = output * x;
+	    }
+ 
+	   System.out.println("X=" + x  + " n="+n);
+	   System.out.println("The power of X is:" + output);
+		
+	}
+
+}
